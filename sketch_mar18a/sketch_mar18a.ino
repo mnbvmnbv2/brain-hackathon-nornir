799fb45414de8bbea6add972142291cebe80ba39
Hjalti <hjaltipeh@gmail.com>
2023-03-18 16:08:01 +0100

    counts moving targets
    In and out of room

--- sketch_mar18a/sketch_mar18a.ino
+++ sketch_mar18a/sketch_mar18a.ino
@@ -1,6 +1,13 @@
 #include <NewPing.h>
+//install dependencies under tools > manage dependencies
 
+//pin and max distance in cm defined
 NewPing sonar(10, 11, 100);
+NewPing sonar2(6, 7, 100);
+
+int visitors = 0;
+bool reset = false;
+
 
 void setup() {
   Serial.begin(9600);
@@ -8,9 +15,36 @@ void setup() {
 }
 
 void loop() {
+
+  //checks which sensor is triggered to determine increast or decrease
+  //reset check ensures no repeat scans in case of still standing target
+  if((sonar.ping_cm() > 0) && (sonar2.ping_cm() == 0) && (reset == true)){
+    visitors++;
+    Serial.println(visitors);
+    reset = false;
+    delay(1000);
+  }
+  else if((sonar.ping_cm() == 0) && (sonar2.ping_cm() > 0) && (reset == true)){
+    visitors--;
+    Serial.println(visitors);
+    reset = false;
+    delay(1000);
+  }
+  else{
+    Serial.print("NOTHING NEW ");
+    Serial.println(visitors);
+    reset = true;
+  }
+
+ 
+  /*
+  // Checks distance
+  Serial.print("1 ");
   Serial.println(sonar.ping_cm());
-  /*if(sonar.ping_cm() > 0) {
-    Serial.println("Someone passed by");
+  delay(100);
+  Serial.print("2 ");
+  Serial.println(sonar2.ping_cm());
   }*/
+
   delay(1000);
 }
