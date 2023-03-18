ed0388462d2449d8bc67438e9e7e749b592b1d3d
Jonas Nordstrøm <jonas.nordst@gmail.com>
2023-03-18 15:32:46 +0100

    updated

--- make model and predict.py
+++ make model and predict.py
@@ -2,24 +2,31 @@
 import pandas as pd
 import xgboost as xgb
 
-def predict_rate(df, model):
-    cat_cols = list(df.select_dtypes(exclude="number").columns)
-    for col in cat_cols:
-        y = pd.get_dummies(df[col], prefix = col)
-        df.drop(col, axis = 1)
-        df.join(y)
+class ArrivalPrediction:
     
-    return model.predict(df)
+    def __init__(self, X, y) -> None:
+        self.totData = X
+        self.totResponse = y
+        self.model = self.make_model(X, y)
+    
+    def predict_rate(self, df):
+        cat_cols = list(df.select_dtypes(exclude="number").columns)
+        for col in cat_cols:
+            y = pd.get_dummies(df[col], prefix = col)
+            df.drop(col, axis = 1)
+            df.join(y)
+        
+        return self.model.predict(df)
 
-def make_model(df, df_y):
-    cat_cols = list(df.select_dtypes(exclude="number").columns)
-    for col in cat_cols:
-        y = pd.get_dummies(df[col], prefix = col)
-        df.drop(col, axis = 1)
-        df.join(y)
-    
-    xgb_model = xgb.XGBRegressor(n_estimates = 1000,
-                             learning_rate = 0.05,
-                             n_jobs = -1)
-    xgb_model.fit(df, df_y)
-    return xgb_model
+    def make_model(self, df, df_y):
+        cat_cols = list(df.select_dtypes(exclude="number").columns)
+        for col in cat_cols:
+            y = pd.get_dummies(df[col], prefix = col)
+            df.drop(col, axis = 1)
+            df.join(y)
+        
+        xgb_model = xgb.XGBRegressor(n_estimates = 1000,
+                                learning_rate = 0.05,
+                                n_jobs = -1)
+        xgb_model.fit(df, df_y)
+        return xgb_model
